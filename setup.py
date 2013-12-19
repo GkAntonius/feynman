c5db59bc80a5545b75151672797ad9cd94866fc0
Gabriel Antonius <gabriel.antonius@gmail.com>
2013-12-18 22:03:36 -0500

    Release 1.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = [
 
 setup_args = dict(
       name             = 'feynman',
-      version          = '0.1.0',
+      version          = '1.0.0',
       description      = "Feynman diagrams with python-matplotlib.",
       author           = "Gabriel Antonius",
       author_email     = "gabriel.antonius@gmail.com",
